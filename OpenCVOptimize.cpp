49deb1e4152546999bf523e3e254228958f7635f
Raghunath Nagaraj <67452178+raghunihal@users.noreply.github.com>
2023-08-17 15:56:38 +0530

    Update OpenCVOptimize.cpp

--- OpenCVOptimize.cpp
+++ OpenCVOptimize.cpp
@@ -4,7 +4,18 @@
 #include <iostream>
 int main()
 {
-  bool optimized_flag = 
+   // Read the optimization settings
+  bool optimized_flag = cv::useOptimized();
+  std::cout<<"Optimized Flag Setting :"<< optimized_flag<< std::endl;
 
-
+  // Set the optimization to false
+  cv::setUseOptimized(false);
+  optimized_flag = cv::useOptimized();
+  std::cout<<"Optimized Flag after setting false :"<< optimized_flag<< std::endl;
+  
+  // Set the optimization to true
+  cv::setUseOptimized(true);
+  optimized_flag = cv::useOptimized();
+  std::cout<<"Optimized Flag after setting true :"<< optimized_flag<< std::endl;
+  return 0;
 }
