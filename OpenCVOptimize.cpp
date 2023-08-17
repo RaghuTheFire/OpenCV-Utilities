4fd13b7ac738e9389331173659cd7685acd399bb
Raghunath Nagaraj <67452178+raghunihal@users.noreply.github.com>
2023-08-17 15:49:22 +0530

    Update OpenCVOptimize.cpp

--- OpenCVOptimize.cpp
+++ OpenCVOptimize.cpp
@@ -1,1 +1,10 @@
+//Usage: g++ OpenCVOptimize.cpp -o OpenCVOptimize `pkg-config --cflags --libs opencv4`
 
+#include <opecv2/opencv.hpp>
+#include <iostream>
+int main()
+{
+  bool optimized_flag = 
+
+
+}
