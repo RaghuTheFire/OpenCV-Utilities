44c8d208d2b6a61a32b4299fb1aaeff80f560bdd
Raghunath Nagaraj <67452178+raghunihal@users.noreply.github.com>
2023-08-17 15:19:56 +0530

    Update OpenCVMeasurePerf.cpp

--- OpenCVMeasurePerf.cpp
+++ OpenCVMeasurePerf.cpp
@@ -1,1 +1,12 @@
+#include <opecv2/opencv.hpp>
+#include <iostream>
+int main()
+{
+  int64 t1 = cv::getCPUTickCount();
+  //Perform Some Opeartion
+  int64 t2 = cv::getCPUTickCount();
 
+  double elapsed_time = (t2-t1)/cv::getTickFrequency();
+  std::cout<<"Elapsed Time:"<<elapsed_time<<"Seconds."<<std::endl;
+  return 0;
+}
